74ec768aba583f324398f04ca9b11226901b2c53
= <sharmamitch@gmail.com>
2022-08-16 16:31:58 -0500

    Implemented event card component Added example events to events page

--- src/components/EventsContent/EventCard.js
+++ src/components/EventsContent/EventCard.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './EventCard.css'
 
-function EventCard() {
+function EventCard(props) {
+  /* Props:
+      title - event title
+      description - event description
+      img - event image
+  */
   return (
-    <div>EventCard</div>
+    <div class="event-card">
+      <div class="event-card-container">
+        <div class="event-img">
+          <img src={props.img} />
+        </div>
+        <div class="event-text">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
