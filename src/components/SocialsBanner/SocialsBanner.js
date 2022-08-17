8ea7d4a7d4b0f6c21cb819874ad2668d36873821
= <=>
2022-08-17 16:22:51 -0500

    Changed row component to support inline tags Row component now takes in objects containing html (rather than strings) Text effects (bold, italic) and inline links (href) can now be used

--- src/components/SocialsBanner/SocialsBanner.js
+++ src/components/SocialsBanner/SocialsBanner.js
@@ -12,7 +12,7 @@ function SocialsBanner() {
     <div class="SocialsBanner">
         <div class="socials-container">
             <a href="https://www.instagram.com/uhsacnas/"><img class="social-item" src={SACNASInsta}/></a>
-            <a href="#TODO"><img class="social-item" src={SACNASDiscord}/></a>
+            <a href="https://discord.gg/KKq7Ygc3p4"><img class="social-item" src={SACNASDiscord}/></a>
             <a href="https://www.facebook.com/University-of-Houston-SACNAS-316313025690469/"><img class="social-item" src={SACNASFacebook}/></a>
             <a href="https://twitter.com/uh_sacnas"><img class="social-item" src={SACNASTwitter}/></a>
             <a href="https://www.linkedin.com/company/82377992"><img class="social-item" src={SACNASLinkedIn}/></a>
