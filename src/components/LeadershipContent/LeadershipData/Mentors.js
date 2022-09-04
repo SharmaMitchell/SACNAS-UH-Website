e795ad827356e670f05da06fca9847d146b5b87a
Mitchell Sharma <sharmamitch@gmail.com>
2022-09-03 22:33:22 -0500

    Added new mentor (alex)

--- src/components/LeadershipContent/LeadershipData/Mentors.js
+++ src/components/LeadershipContent/LeadershipData/Mentors.js
@@ -1,12 +1,18 @@
 import React from "react";
 import rosh from '../../../assets/leadership/rosh.png'
-
+import alex from '../../../assets/leadership/alex.png'
 
 export const Mentors = [
     {
         img: rosh,
         title: "Roshni Jaffery: Graduate Officer Mentor",
         paragraph: "Roshni is a third year graduate student at the University of Houston pursuing a PhD in Cell and Molecular Biology. Roshni received her undergraduate degree from Baylor University. She works in Dr. Weiyi Peng’s lab at UH and works on several projects relating to immunology. Roshni is a SACNAS officer committed to increasing minority awareness in the STEM field.",
+        link: ""
+    },
+    {
+        img: alex,
+        title: "Alex Ferrer: Graduate Officer Mentor",
+        paragraph: "Alex is a second year graduate student in the department of Biology and Biochemistry and is part of the Cell and Molecular Biology program. He is a neuroendocrinologist in Dr. Beau Alward’s lab interested in how steroid hormones affect gene expression in the brain and how this gene expression changes behavioral phenotypes. He joined SACNAS intending to help all students, no matter their background, find a fulfilling school/research experience.",
         link: ""
     },
 ]
