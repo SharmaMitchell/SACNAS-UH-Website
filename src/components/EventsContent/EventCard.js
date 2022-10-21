b39f350ce4ef88303d314504cda263a78c856b91
Mitchell Sharma <sharmamitch@gmail.com>
2022-10-21 12:23:02 -0500

    Added datetime and location info to event cards

--- src/components/EventsContent/EventCard.js
+++ src/components/EventsContent/EventCard.js
@@ -5,8 +5,24 @@ function EventCard(props) {
   /* Props:
       title - event title
       description - event description
+      location - event location
+      date - event date
+      time - time of event
       img - event image
+      link1Label - label for link 1 (optional)
+      link1 - link 1 (event-related URL, ex. sign-up form, RSVP)
+      link2Label - label for linl 2
+      link2 - link 2
   */
+  console.log(props);
+  let link1Tag = ``;
+  let link2Tag = ``;
+  if (props.link1 != undefined && props.link1 != ""){
+  link1Tag = (<a href={props.link1}>{props.link1Label}</a>);
+ }
+ if (props.link2 != undefined && props.link2 != ""){
+  link2Tag = (<a href={props.link2}>{props.link2Label}</a>);
+ }
   return (
     <div class="event-card">
       <div class="event-card-container">
@@ -15,8 +31,22 @@ function EventCard(props) {
         </div>
         <div class="event-text">
           <h3>{props.title}</h3>
-          <p>{props.description}</p>
+          <div class="event-datetime">
+            {props.time ? <p>{props.time}</p> : <></>}
+            {/* {props.time && props.date ? <p>, </p> : <></>} */}
+            {props.date ? <p>{props.date}</p> : <></>}
+          </div>
+          <div class="event-location">
+            {props.location ? <p>{props.location}</p> : <></>}
+          </div>
+          <div class="event-links">
+            {link1Tag}
+            {link2Tag}
+          </div>
+          <p class="event-description">{props.description}</p>
         </div>
+        
+        
       </div>
     </div>
   )
--- src/components/EventsContent/EventsContent.js
+++ src/components/EventsContent/EventsContent.js
@@ -7,17 +7,17 @@ function EventsContent() {
   /* Public Google Sheet ID */
   const SHEET_ID = '19AWlwwjbWWRGz68mzYSHN5-mKiKmKZbHZDuDpZegtmE';
   /* Read-only API key, limited to sacnas-uh.org domain */
-  const API_KEY = 'AIzaSyCWFLx8b9hgh5nlwhN_9S6awfghwUBoXLo';
+  const API_KEY = 'AIzaSyCIRgSXY0QOyGLN6jqOfFNKZ0He445L99o'; // DEV API KEY!!!!!!!!!!!!!
   
   const [upcoming, setUpcoming] = useState([]);
   const [uLoading, setULoading] = useState(true);
 
-  useEffect(() => {fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Upcoming!A2:G18?key=${API_KEY}`)
+  useEffect(() => {fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Upcoming!A2:J18?key=${API_KEY}`)
     .then(function(response){
       return response.json();
     })
     .then(function(data){
-      /*console.log(data.values);*/
+      console.log(data.values);
       let eventData = data.values;
       setUpcoming(data.values);
       setULoading(false);
@@ -27,12 +27,12 @@ function EventsContent() {
   const [previous, setPrevious] = useState([]);
   const [pLoading, setPLoading] = useState(true);
 
-  useEffect(() => {fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Archive!A2:G18?key=${API_KEY}`)
+  useEffect(() => {fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Archive!A2:J18?key=${API_KEY}`)
     .then(function(response){
       return response.json();
     })
     .then(function(data){
-      /*console.log(data.values);*/
+      console.log(data.values);
       let eventData = data.values;
       setPrevious(data.values);
       setPLoading(false);
@@ -47,7 +47,19 @@ function EventsContent() {
           <div class="event-list">
             
             {uLoading ? <Spinner /> : upcoming?.map((event) => {
-              return(<EventCard title={event[0]} description={event[1]} img={event[5]}/>)
+              return(
+                <EventCard 
+                  title={event[0]} 
+                  description={event[1]} 
+                  location={event[2]}
+                  date={event[3]}
+                  time={event[4]}
+                  img={event[5]}
+                  link1Label={event[6]}
+                  link1={event[7]}
+                  link2Label={event[8]}
+                  link2={event[9]}
+                />)
             })}
             
           </div>
@@ -56,7 +68,18 @@ function EventsContent() {
           <h2>Previous Events</h2>
           <div class="event-list">
             {pLoading ? <Spinner /> : previous?.map((event) => {
-              return(<EventCard title={event[0]} description={event[1]} img={event[5]}/>)
+              return(<EventCard 
+                title={event[0]} 
+                description={event[1]} 
+                location={event[2]}
+                date={event[3]}
+                time={event[4]}
+                img={event[5]}
+                link1Label={event[6]}
+                link1={event[7]}
+                link2Label={event[8]}
+                link2={event[9]}
+              />)
             })}
           </div>
         </div>
