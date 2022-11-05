03cb78087996116a8e2f81c32d33809652f4e5cb
Mitchell Sharma <sharmamitch@gmail.com>
2022-11-04 19:21:14 -0500

    Updated Lorissa pic, Marcela & Alejandro roles Removed Alex, added Daniel to Alumni

--- src/components/LeadershipContent/LeadershipData/Mentors.js
+++ src/components/LeadershipContent/LeadershipData/Mentors.js
@@ -1,18 +1,17 @@
-import React from "react";
 import rosh from '../../../assets/leadership/rosh.png'
-import alex from '../../../assets/leadership/alex.png'
+import alejandro from '../../../assets/leadership/alejandro.jpg'
 
 export const Mentors = [
+    {
+        img: alejandro,
+        title: "Alejandro Ramirez: Graduate Officer Mentor",
+        paragraph: "Alejandro is a fourth year graduate student working towards a PhD in experimental astroparticle physics. Working under Dr. Andrew Renshaw, his research involves developing and surveying particle detector technologies for DarkSide 20k, a dark matter experiment searching for Weakly Interacting Massive Particles. Alejandro is a co-founder of the SACNAS UH chapter and aims to help students of all backgrounds find a fulfilling research experience.",
+        link: ""
+    },
     {
         img: rosh,
         title: "Roshni Jaffery: Graduate Officer Mentor",
         paragraph: "Roshni is a third year graduate student at the University of Houston pursuing a PhD in Cell and Molecular Biology. Roshni received her undergraduate degree from Baylor University. She works in Dr. Weiyi Peng’s lab at UH and works on several projects relating to immunology. Roshni is a SACNAS officer committed to increasing minority awareness in the STEM field.",
-        link: ""
-    },
-    {
-        img: alex,
-        title: "Alex Ferrer: Graduate Officer Mentor",
-        paragraph: "Alex is a second year graduate student in the department of Biology and Biochemistry and is part of the Cell and Molecular Biology program. He is a neuroendocrinologist in Dr. Beau Alward’s lab interested in how steroid hormones affect gene expression in the brain and how this gene expression changes behavioral phenotypes. He joined SACNAS intending to help all students, no matter their background, find a fulfilling school/research experience.",
         link: ""
     },
 ]
