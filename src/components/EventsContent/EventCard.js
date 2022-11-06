a6e3d83777e7c2dd7f82f103eefa4623e188a43c
Mitchell Sharma <sharmamitch@gmail.com>
2022-11-06 01:02:31 -0600

    Event card redesign: expanding event cards with more info Added smooth "view more" button & expansion animation to event cards

--- src/components/EventsContent/EventCard.js
+++ src/components/EventsContent/EventCard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link }from 'react-router-dom'
 import './EventCard.css'
 import expand from '../../assets/expand.png'
 
@@ -15,7 +16,11 @@ function EventCard(props) {
       link2Label - label for linl 2
       link2 - link 2
   */
-  console.log(props);
+  // console.log(props);
+
+  const [viewMore, setViewMore] = useState(false);
+  const toggleViewMore = () => setViewMore(!viewMore);
+
   let link1Tag = ``;
   let link2Tag = ``;
   if (props.link1 != undefined && props.link1 != ""){
@@ -25,38 +30,41 @@ function EventCard(props) {
   link2Tag = (<a href={props.link2}>{props.link2Label}</a>);
  }
 
- let moreEventInfo = `<div class="event-datetime">
- {props.time ? <p>{props.time}</p> : <></>}
- {/* {props.time && props.date ? <p>, </p> : <></>} */}
- {props.date ? <p>{props.date}</p> : <></>}
-</div>
-<div class="event-location">
- {props.location ? <p>{props.location}</p> : <></>}
-</div>
-<div class="event-links">
- {link1Tag}
- {link2Tag}
-</div>`;
-
   return (
     <div class="event-card">
-      <div class="event-card-container">
-        <div class="event-img">
-          <img src={props.img} />
+      <Link to="#" class="event-card-link-wrapper">
+        <div class="event-card-container" onClick={toggleViewMore}>
+          <div class="event-img">
+            <img src={props.img} />
+          </div>
+          <div class={viewMore ? "event-text full" : "event-text"}>
+            <h3>{props.title}</h3>
+            <p class="event-description">{props.description}</p>
+            <div class="event-more-info">
+              <div class="event-location">
+                {props.location ? <p><b>Location:</b> {props.location}</p> : <></>}
+              </div>
+              <div class="event-datetime">
+                {props.time ? <p>{props.time}</p> : <></>}
+                {/* {props.time && props.date ? <p>, </p> : <></>} */}
+                {props.date ? <p>{props.date}</p> : <></>}
+              </div>
+              <div class="event-links">
+                {link1Tag}
+                {link2Tag}
+              </div>
+            </div>
+          </div>
+          <div class="event-view-more-bg">
+            <a class="event-view-more-button">
+              View {viewMore ? "Less" : "More"}
+              <img class={viewMore ? "event-view-more-expand active" : "event-view-more-expand"} src={expand} />
+            </a>
+          </div>
         </div>
-        <div class="event-text">
-          <h3>{props.title}</h3>
-          <p class="event-description">{props.description}</p>
-        </div>
-        <div class="event-view-more-bg">
-          <a class="event-view-more-button">
-            View More
-            <img class="event-view-more-expand" src={expand}/>
-          </a>
-        </div>
-      </div>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default EventCard
