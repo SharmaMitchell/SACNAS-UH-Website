759a26f9d60ef9e71f29f52f5ce9fbc700502a89
Mitchell Sharma <sharmamitch@gmail.com>
2022-11-06 19:24:43 -0600

    Leadership spelling corrections

--- src/components/LeadershipContent/LeadershipData/AlumniAdvisors.js
+++ src/components/LeadershipContent/LeadershipData/AlumniAdvisors.js
@@ -6,13 +6,13 @@ export const AlumniAdvisors = [
     {
         img: steven,
         title: "Steven Nguyen: Alumni Advisor",
-        paragraph: "Steven graduated from UH in 2022 with a BS in Biology. He is currently working on a project to uncover the role of long non-coding RNAs in castration-resistant prostate cancer in Dr. Qin Feng’s lab. Steven plans to apply soon to do a PhD and continue to pursue research. He joined SACNAS for its plethora of diverse members, graduate mentors, and professional resources. He joined the Alumni Advisory Board to contribute to the organization’s success. He will be joining the lab of Dr. Ping Yi as a laboratory technician in Fall 2022.",
+        paragraph: "Steven graduated from UH in 2022 with a B.S. in Biology. He is currently working on a project to uncover the role of long non-coding RNAs in castration-resistant prostate cancer in Dr. Qin Feng’s lab. Steven plans to apply soon to do a PhD and continue to pursue research. He joined SACNAS for its plethora of diverse members, graduate mentors, and professional resources. He joined the Alumni Advisory Board to contribute to the organization’s success. He will be joining the lab of Dr. Ping Yi as a laboratory technician in Fall 2022.",
         link: ""
     },
     {
         img: jennifer,
         title: "Jennifer Ruiz: Alumni Advisor",
-        paragraph: "Jennifer graduated from UH in 2022 with a BS in Biology. She has been a member of the Pennings ecology lab group since 2019. She has experience doing field work at the UH Coastal Center and at the University of Georgia Marine Institute on Sapelo Island. Jennifer is also proficient in using R to conduct data analyses. She hopes to share her experiences being a first generation, Latina scientist to encourage young women and other underrepresented minorities to pursue careers in STEM.",
+        paragraph: "Jennifer graduated from UH in 2022 with a B.S. in Biology. She has been a member of the Pennings ecology lab group since 2019. She has experience doing field work at the UH Coastal Center and at the University of Georgia Marine Institute on Sapelo Island. Jennifer is also proficient in using R to conduct data analysis. She hopes to share her experiences being a first generation, Latina scientist to encourage young women and other underrepresented minorities to pursue careers in STEM.",
         link: ""
     },
 ]
--- src/components/LeadershipContent/LeadershipData/Officers.js
+++ src/components/LeadershipContent/LeadershipData/Officers.js
@@ -13,31 +13,31 @@ export const Officers = [
     {
         img: lorissa,
         title: "Lorissa Saiz: President",
-        paragraph: "Lorissa is a third year graduate student in the department of Biology and Biochemistry, and is a part of the Cell and Molecular Biology program. She is an evolutionary geneticist in Dr. Erin Kelleher’s lab interested in uncovering causative genotypes that result in transposable element tolerance. Lorissa is one of the founding members of the UH SACNAS chapter and is dedicated to increasing minority representation in STEM and giving women a voice in science. She also aims to improve science communication between academics and the public through the chapter.",
+        paragraph: "Lorissa is a third year graduate student in the Department of Biology and Biochemistry, and is a part of the Cell and Molecular Biology program. She is an evolutionary geneticist in Dr. Erin Kelleher’s lab interested in uncovering causative genotypes that result in transposable element tolerance. Lorissa is one of the founding members of the UH SACNAS chapter and is dedicated to increasing minority representation in STEM and giving women a voice in science. She also aims to improve science communication between academics and the public through the chapter.",
         link: ""
     },
     {
         img: marcela,
         title: "Marcela Strane: Vice President",
-        paragraph: "Marcela is a second year PhD student in Environmental Engineering researching under Dr. Devin Shaffer. Her research focuses on microplastics pollution from biosolids leaving waste water treatments plants applied to agrosystems and their fate and transport into wetlands. Marcela is dedicated to making STEAM a more diverse and inclusive space by directly volunteering in K-12 outreach in the Houston area. She loves bringing awareness to some of the issues the gulf coast faces and how people can be citizen scientists. She recently joined the SACNAS UH Chapter the summer of 2022",
+        paragraph: "Marcela is a second year PhD student in Environmental Engineering researching under Dr. Devin Shaffer. Her research focuses on microplastics pollution from biosolids leaving waste water treatment plants applied to agrosystems and their fate and transport into wetlands. Marcela is dedicated to making STEAM a more diverse and inclusive space by directly volunteering in K-12 outreach in the Houston area. She loves bringing awareness to some of the issues the Gulf Coast faces and how people can be citizen scientists. She recently joined the SACNAS UH Chapter the summer of 2022",
         link: ""
     },
     {
         img: jake,
         title: "Jacob Steele: Graduate Financial Officer",
-        paragraph: "Jake is a third year graduate student in the department of Biology and Biochemistry and is pursuing a PhD in Biochemistry. He received his BS in Biochemistry from Florida Gulf Coast University. Jake is currently a member of Dr. Yuhong Wang’s lab at UH and his primary research project focuses on understanding ribosomal translocation during mRNA translation. Jake has been a SACNAS officer since the Fall of 2020 and strives to further the diversification of STEM related disciplines.",
+        paragraph: "Jake is a third year graduate student in the Department of Biology and Biochemistry and is pursuing a PhD in Biochemistry. He received his B.S. in Biochemistry from Florida Gulf Coast University. Jake is currently a member of Dr. Yuhong Wang’s lab at UH and his primary research project focuses on understanding ribosomal translocation during mRNA translation. Jake has been a SACNAS officer since the Fall of 2020 and strives to further the diversification of STEM related disciplines.",
         link: ""
     },
     {
         img: yuni,
         title: "Yunuen Avila Martinez: Graduate Officer",
-        paragraph: "Yunuen is a second year graduate student in the department of chemistry and is pursuing her PhD working under Dr. Brad Carrow. Her research focuses on synthesizing sustainable polymers and in up-cycling existing plastics. Yunuen recently joined SACNAS because she enjoyed the atmosphere of the group and felt supported by the friends she made here. Her goal now is to provide an environment where STEM grads and undergrads of all backgrounds can feel comfortable and encouraged in their journey through college. She also hopes to influence interactions between different STEM fields to diversify our points of view.",
+        paragraph: "Yunuen is a second year graduate student in the Department of Chemistry and is pursuing her PhD working under Dr. Brad Carrow. Her research focuses on synthesizing sustainable polymers and in up-cycling existing plastics. Yunuen recently joined SACNAS because she enjoyed the atmosphere of the group and felt supported by the friends she made here. Her goal now is to provide an environment where STEM grads and undergrads of all backgrounds can feel comfortable and encouraged in their journey through college. She also hopes to influence interactions between different STEM fields to diversify our points of view.",
         link: ""
     },
     {
         img: an,
         title: "An Bui: Undergraduate Financial Officer",
-        paragraph: "An is a Junior undergraduate Biotechnology (Bioinformatics track) major at the University of Houston. She currently does research with Lorissa in the Kelleher Lab. She plans to go to grad school to either get a masters in biotechnology or in cell molecular biology. Coding is one of her hobbies that she enjoys and is able to code in C++. An LOVES science fiction and will non-stop talk about what the future may be. An hopes to bring awareness that minorities can succeed in STEM, and inspire others that could be discouraged due to their skin or background through her work in the chapter.",
+        paragraph: "An is a Junior Undergraduate Biotechnology (Bioinformatics track) major at the University of Houston. She currently does research with Lorissa in the Kelleher Lab. She plans to go to grad school to either get a masters in biotechnology or in cell molecular biology. Coding is one of her hobbies that she enjoys and is able to code in C++. An loves science fiction and will non-stop talk about what the future may be. An hopes to bring awareness that minorities can succeed in STEM, and inspire others that could be discouraged due to their skin or background through her work in the chapter.",
         link: ""
     },
     {
@@ -49,13 +49,13 @@ export const Officers = [
     {
         img: peanut,
         title: "Vuong (Peanut) Vu: Undergraduate Officer - Secretary",
-        paragraph: "Vuong (Peanut) Vu is a sophomore at the University of Houston majoring in Psychology with a Biology minor. He is currently interested in pursuing an MD in hopes of becoming an anesthesiologist or radiologist. As of late, he is working with houseflies in Dr. Richard Meisel’s laboratory studying polygenic sex determination. He joined SACNAS in hopes of increasing minorities in STEM and to give them opportunities to excel in life.",
+        paragraph: "Vuong (Peanut) Vu is a Sophomore at the University of Houston majoring in Psychology with a Biology minor. He is currently interested in pursuing an MD in hopes of becoming an anesthesiologist or radiologist. As of late, he is working with houseflies in Dr. Richard Meisel’s laboratory studying polygenic sex determination. He joined SACNAS in hopes of increasing minorities in STEM and to give them opportunities to excel in life.",
         link: ""
     },
     {
         img: sara,
         title: "Sara Rashid: Undergraduate Officer",
-        paragraph: "Sara is a senior majoring in Biochemistry with a Spanish Language minor at the University of Houston, who is currently hoping to pursue a PhD in Biochemistry. Her interests include molecular medicine and drug discovery, and has always carried a passion for medical advocacy, particularly for women of color. Initially drawn to SACNAS for its active community and professional resources, Sara now seeks to aid others of minority backgrounds in their own pursuits in science. Additionally, she wishes to assist in spreading the organization’s messages of the importance of diversity and inclusivity in STEM.",
+        paragraph: "Sara is a Senior majoring in Biochemistry with a Spanish Language minor at the University of Houston, who is currently hoping to pursue a PhD in Biochemistry. Her interests include molecular medicine and drug discovery, and she has always carried a passion for medical advocacy, particularly for women of color. Initially drawn to SACNAS for its active community and professional resources, Sara now seeks to aid others of minority backgrounds in their own pursuits in science. Additionally, she wishes to assist in spreading the organization’s messages of the importance of diversity and inclusivity in STEM.",
         link: ""
     },
     {
