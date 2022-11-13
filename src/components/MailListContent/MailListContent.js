61bd5379b2d5a52ddd3ea5374be0ec47d30e2f9a
Mitchell Sharma <sharmamitch@gmail.com>
2022-11-13 03:23:30 -0600

    Improved mail list page responsiveness Unfortunately had to hardcode some scaling since iframes suck

--- src/components/MailListContent/MailListContent.js
+++ src/components/MailListContent/MailListContent.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './MailListContent.css'
 
+function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+}
+
 function MailListContent() {
+  
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+  
+  let iFrameHeight = "1750";
+  if (windowSize.innerWidth <= 320){
+    iFrameHeight = "2500";
+  }
+  else if (windowSize.innerWidth <= 380){
+    iFrameHeight = "2200";
+  }
+  else if (windowSize.innerWidth <= 500){
+    iFrameHeight = "2000";
+  }
+  else if (windowSize.innerWidth < 768){
+    iFrameHeight = "1850";
+  }
+
   return (
     <div>
       <div class="gform-wrapper">
@@ -9,10 +43,11 @@ function MailListContent() {
             class="gform"
             src="https://docs.google.com/forms/d/e/1FAIpQLScaahgIdex5tTTZnRiWNXglj62eJ4t9Ut505KCtjWuo55IqTw/viewform?embedded=true"
             // width="700"
-            height="1750"
+            height={iFrameHeight}
             frameborder="0"
             marginheight="0"
             marginwidth="0"
+            // scrolling='no'
         >
             Loading…
         </iframe>
