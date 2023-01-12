15ec1a52b506deb8307ffc37dcc354cbbdabe4bf
Mitchell Sharma <sharmamitch@gmail.com>
2023-01-12 02:55:15 -0600

    Added event location & add to calendar buttons on hover

--- src/components/EventsContent/EventCard.js
+++ src/components/EventsContent/EventCard.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { Link }from 'react-router-dom'
 import './EventCard.css'
 import expand from '../../assets/expand.png'
+import zoom from '../../assets/zoom.png'
+import maps from '../../assets/maps.png'
+import calendar from '../../assets/calendar.png'
+import linkImg from '../../assets/link.png'
 
 function EventCard(props) {
   /* Props:
@@ -33,37 +37,49 @@ function EventCard(props) {
 
   return (
     <div class="event-card">
-      <Link to="#" class="event-card-link-wrapper">
-        <div class="event-card-container" onClick={toggleViewMore}>
+      {/* <Link to="#" class="event-card-link-wrapper"> */}
+        <div class="event-card-container">
           <div class="event-img">
-            <img src={props.img} />
+            <div class="event-img-overlay">
+              <div class="overlay-row">
+                <a href="#"><img src={zoom}/></a>
+                {link1Tag != '' ? <a target="_blank" rel="noopener" href={props.link1}><img src={linkImg}/></a> : <></>}
+              </div>
+              <div class="overlay-row">
+                <a target="_blank" rel="noopener" href={`https://www.google.com/calendar/render?action=TEMPLATE&text=${props.title}&dates=${props.date}&details=${props.description}&location=${props.location}&sf=true&output=xml`}><img src={calendar}/></a>
+                <a target="_blank" rel="noopener" href={`https://www.google.com/maps/search/?api=1&query=${props.location}`}><img src={maps}/></a>
+              </div>
+            </div> 
+            <img class="poster" src={props.img} />
           </div>
-          <div class={viewMore ? "event-text full" : "event-text"}>
-            <h3>{props.title}</h3>
-            <p class="event-description">{props.description}</p>
-            <div class="event-more-info">
-              <div class="event-location">
-                {props.location ? <p><b>Location:</b> {props.location}</p> : <></>}
-              </div>
-              <div class="event-datetime">
-                {props.time ? <p>{props.time}</p> : <></>}
-                {/* {props.time && props.date ? <p>, </p> : <></>} */}
-                {props.date ? <p>{props.date}</p> : <></>}
-              </div>
-              <div class="event-links">
-                {link1Tag}
-                {link2Tag}
+          <a onClick={toggleViewMore} class="event-card-link-wrapper">
+            <div class={viewMore ? "event-text full" : "event-text"}>
+              <h3>{props.title}</h3>
+              <p class="event-description">{props.description}</p>
+              <div class="event-more-info">
+                <div class="event-location">
+                  {props.location ? <p><b>Location:</b> {props.location}</p> : <></>}
+                </div>
+                <div class="event-datetime">
+                  {props.time ? <p>{props.time}</p> : <></>}
+                  {/* {props.time && props.date ? <p>, </p> : <></>} */}
+                  {props.date ? <p>{props.date}</p> : <></>}
+                </div>
+                <div class="event-links">
+                  {link1Tag}
+                  {link2Tag}
+                </div>
               </div>
             </div>
-          </div>
-          <div class="event-view-more-bg">
-            <a class="event-view-more-button">
-              View {viewMore ? "Less" : "More"}
-              <img class={viewMore ? "event-view-more-expand active" : "event-view-more-expand"} src={expand} />
-            </a>
-          </div>
+            <div class="event-view-more-bg">
+              <a class="event-view-more-button">
+                View {viewMore ? "Less" : "More"}
+                <img class={viewMore ? "event-view-more-expand active" : "event-view-more-expand"} src={expand} />
+              </a>
+            </div>
+          </a>
         </div>
-      </Link>
+      {/* </Link> */}
     </div>
   );
 }
