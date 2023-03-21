67753b76be49b414b4adaf3942c9371ea359b58a
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-20 21:46:09 -0500

    Updated resources component, added todos

--- src/components/ResourcesContent/ResourcesContent.js
+++ src/components/ResourcesContent/ResourcesContent.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+/* 
+TODO:
+- Get 5 latest videos from SACNAS UH YouTube channel
+- Link (or embed) the videos
+- List video metadata (title, description, date published) next to the link/embed 
+    (if possible via youtube API)
+- Add a link to the SACNAS UH YouTube channel (May want to add that to the footer too)
+*/
+
 function ResourcesContent() {
   return (
     <div>ResourcesContent</div>
