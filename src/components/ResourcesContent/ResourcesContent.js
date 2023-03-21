099d4a96f099f3e76cfb586972f2575a5c28bd61
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-20 21:51:58 -0500

    Implemented resources component, added placeholder video data

--- src/components/ResourcesContent/ResourcesContent.js
+++ src/components/ResourcesContent/ResourcesContent.js
@@ -9,9 +9,71 @@ TODO:
 - Add a link to the SACNAS UH YouTube channel (May want to add that to the footer too)
 */
 
+const videos = [
+    {
+        title: "SACNAS UH: 2021 Virtual Graduation",
+        description: "SACNAS UH 2021 Virtual Graduation",
+        date: "May 2021",
+        link: "https://www.youtube.com/watch?v=6I0J0l9c6gA"
+    },
+    {
+        title: "SACNAS UH: 2021 Virtual Graduation",
+        description: "SACNAS UH 2021 Virtual Graduation",
+        date: "May 2021",
+        link: "https://www.youtube.com/watch?v=6I0J0l9c6gA"
+    },
+    {
+        title: "SACNAS UH: 2021 Virtual Graduation",
+        description: "SACNAS UH 2021 Virtual Graduation",
+        date: "May 2021",
+        link: "https://www.youtube.com/watch?v=6I0J0l9c6gA"
+    },
+    {
+        title: "SACNAS UH: 2021 Virtual Graduation",
+        description: "SACNAS UH 2021 Virtual Graduation",
+        date: "May 2021",
+        link: "https://www.youtube.com/watch?v=6I0J0l9c6gA"
+    },
+    {
+        title: "SACNAS UH: 2021 Virtual Graduation",
+        description: "SACNAS UH 2021 Virtual Graduation",
+        date: "May 2021",
+        link: "https://www.youtube.com/watch?v=6I0J0l9c6gA"
+    },
+]
+
 function ResourcesContent() {
   return (
-    <div>ResourcesContent</div>
+    <div class="resources">
+            <div class="resources-container">
+                <div class="resources-title">
+                    <h1>Resources</h1>
+                </div>
+                <div class="resources-content">
+                    <div class="resources-content-container">
+                        <div class="resources-content-title">
+                            <h2>SACNAS UH YouTube</h2>
+                            {videos.map((video) => (
+                                <div class="resources-content-video">
+                                    <div class="resources-content-video-title">
+                                        <h3>{video.title}</h3>
+                                    </div>
+                                    <div class="resources-content-video-description">
+                                        <p>{video.description}</p>
+                                    </div>
+                                    <div class="resources-content-video-date">
+                                        <p>{video.date}</p>
+                                    </div>
+                                    < div class="resources-content-video-link">
+                                        <a href={video.link}>Link</a>
+                                        </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
