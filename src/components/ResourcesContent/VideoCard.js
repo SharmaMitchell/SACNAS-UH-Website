dd3c73ba606cc787d52719f76d784fd982890a01
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-23 22:28:52 -0500

    Styled video card component Added "View on YouTube" button to card bottom

--- src/components/ResourcesContent/VideoCard.js
+++ src/components/ResourcesContent/VideoCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./VideoCard.css";
+import expand from "../../assets/expand.png";
 
 function VideoCard(props) {
   return (
@@ -14,6 +15,12 @@ function VideoCard(props) {
         <div class="video-card-date">
           <p>{props.date}</p>
         </div>
+      </div>
+      <div class="video-card-view">
+        <a class="video-card-view-button" href={props.link}>
+          View on YouTube
+          <img class="video-card-view-arrow" src={expand} />
+        </a>
       </div>
     </div>
   );
