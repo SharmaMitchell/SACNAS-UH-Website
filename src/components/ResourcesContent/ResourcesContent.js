325a38ae801ee662e5357060abb5624ebd8c800b
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-23 23:49:09 -0500

    Removed placeholder articles

--- src/components/ResourcesContent/ResourcesContent.js
+++ src/components/ResourcesContent/ResourcesContent.js
@@ -7,29 +7,29 @@ TODO:
 - Add a link to the SACNAS UH YouTube channel (May want to add that to the footer too)
 */
 
-const articles = [
-  {
-    title: "Software Engineering Resume Essentials",
-    date: "May 2023",
-    link: "/article/software-engineering-resume-essentials",
-    img: "https://images.unsplash.com/photo-1602407294553-6ac9170b3ed0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    author: "Mitchell Sharma",
-  },
-  {
-    title: "How to get your first internship in STEM",
-    date: "May 2023",
-    link: "/article/how-to-get-your-first-internship-in-stem",
-    img: "https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1447&q=80",
-    author: "Mitchell Sharma",
-  },
-  {
-    title: "Guide to Undergraduate Research",
-    date: "May 2023",
-    link: "/article/guide-to-undergraduate-research",
-    img: "https://plus.unsplash.com/premium_photo-1661378532596-25c6f32ab3d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    author: "An Bui",
-  },
-];
+// const articles = [
+//   {
+//     title: "Software Engineering Resume Essentials",
+//     date: "May 2023",
+//     link: "/article/software-engineering-resume-essentials",
+//     img: "https://images.unsplash.com/photo-1602407294553-6ac9170b3ed0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+//     author: "Mitchell Sharma",
+//   },
+//   {
+//     title: "How to get your first internship in STEM",
+//     date: "May 2023",
+//     link: "/article/how-to-get-your-first-internship-in-stem",
+//     img: "https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1447&q=80",
+//     author: "Mitchell Sharma",
+//   },
+//   {
+//     title: "Guide to Undergraduate Research",
+//     date: "May 2023",
+//     link: "/article/guide-to-undergraduate-research",
+//     img: "https://plus.unsplash.com/premium_photo-1661378532596-25c6f32ab3d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+//     author: "An Bui",
+//   },
+// ];
 
 const SACNAS_UH_YT_ID = "UC2X1nE_E-cpXlSXWKvaRCtQ";
 const NUM_VIDEOS = 6;
@@ -53,7 +53,7 @@ function ResourcesContent() {
   return (
     <div class="resources">
       <div class="resources-container">
-        <div class="resources-title">
+        {/* <div class="resources-title">
           <h2>Articles</h2>
         </div>
         <div class="resources-content">
@@ -68,7 +68,7 @@ function ResourcesContent() {
               />
             ))}
           </div>
-        </div>
+        </div> */}
         <div class="resources-title">
           <h2>Workshops</h2>
         </div>
