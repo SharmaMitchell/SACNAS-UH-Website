d3cafc1b193af6d3e8a481b7368de389c2699a33
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-24 19:46:35 -0500

    Implemented fallback image for articles

--- src/components/ResourcesContent/ArticleCard.js
+++ src/components/ResourcesContent/ArticleCard.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ArticleCard.css";
 import expand from "../../assets/expand.png";
 
 function ArticleCard(props) {
+  const [imgURL, setImgURL] = useState(props.img);
+  const defaultImg = "/assets/articles/defaultthumbnail.jpg";
+
   const formattedDate = new Date(props.date).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
   });
+
+  useEffect(() => {
+    console.log(props.img);
+    if (props.img === "" || props.img === undefined) {
+      setImgURL(defaultImg);
+      console.log("default");
+    }
+  }, [props.img]);
+
   return (
     <div class="article-card">
       <div class="article-card-container">
         <a class="article-card-image" href={props.link}>
-          <img src={props.img} alt="Article Thumbnail" />
+          <img
+            src={imgURL}
+            onError={() => {
+              setImgURL(defaultImg);
+            }}
+            alt="Article Thumbnail"
+            referrerpolicy="no-referrer"
+          />
         </a>
         <a class="article-card-title" href={props.link}>
           <h3>{props.title}</h3>
