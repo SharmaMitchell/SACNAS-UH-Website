be75f1c42259f2969ff90c32505170036955cac9
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-24 20:00:12 -0500

    Added fallback videos for workshops (in case of API limit)

--- src/components/ResourcesContent/ResourcesContent.js
+++ src/components/ResourcesContent/ResourcesContent.js
@@ -18,6 +18,74 @@ const fields = "files(id, name, createdTime, thumbnailLink, mimeType)";
 function ResourcesContent() {
   const [videos, setVideos] = useState([]);
   const [videosLoading, setVideosLoading] = useState(true);
+  const fallbackVideos = [
+    {
+      snippet: {
+        title:
+          "Gilman Scholarship Info Webinar w/Previous Scholarship Recipient hosted by SACNAS UH",
+        description:
+          "Gilman Scholarship Info Webinar: https://www.gilmanscholarship.org",
+        publishedAt: "2022-11-29T00:00:00.000Z",
+      },
+      id: {
+        videoId: "ku1-LGhb_2I",
+      },
+    },
+    {
+      snippet: {
+        title:
+          "SACNAS-UH Mentorship: STEM Graduate Student Experiences Panel 2022",
+        description: "Recording of the mentorship meeting on September 9, 2022",
+        publishedAt: "2022-09-09T00:00:00.000Z",
+      },
+      id: {
+        videoId: "R42JLpnkLJE",
+      },
+    },
+    {
+      snippet: {
+        title: "Graduate Application Materials - SACNAS UH 2022",
+        description: "",
+        publishedAt: "2022-08-12T00:00:00.000Z",
+      },
+      id: {
+        videoId: "vHXIaWYpQn0",
+      },
+    },
+    {
+      snippet: {
+        title: "SACNAS + CAPS Mental Health Awareness",
+        description: "",
+        publishedAt: "2021-09-10T00:00:00.000Z",
+      },
+      id: {
+        videoId: "Ku7iWY_zsKI",
+      },
+    },
+    {
+      snippet: {
+        title:
+          "SACNAS UH Mentorship Program Series - CVs Resumes and Interviews",
+        description: "",
+        publishedAt: "2021-08-09T00:00:00.000Z",
+      },
+      id: {
+        videoId: "K4WpaYvmHuk",
+      },
+    },
+    {
+      snippet: {
+        title: "NSM SACNAS Grad School Workshop",
+        description:
+          "Advice and pointers about applying to and surviving graduate school from our very own UH SACNAS Graduate Committee, and the Chair of Graduate Affairs for Biology and Biochem at UH",
+        publishedAt: "2020-09-13T00:00:00.000Z",
+      },
+      id: {
+        videoId: "ind9m5EZuSQ",
+      },
+    },
+  ];
+
   const [articles, setArticles] = useState([]);
   const [thumnails, setThumbnails] = useState([]);
   const [articlesLoading, setArticlesLoading] = useState(true);
@@ -30,7 +98,11 @@ function ResourcesContent() {
     )
       .then((response) => response.json())
       .then((data) => {
-        setVideos(data.items);
+        if (data.items === undefined) {
+          setVideos(fallbackVideos);
+        } else {
+          setVideos(data.items);
+        }
         setVideosLoading(false);
       });
   }, []);
