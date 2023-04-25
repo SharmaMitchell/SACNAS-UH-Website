5e9d0fd9bc96ecf9b3ef644d46e3b8a110279b74
Mitchell Sharma <sharmamitch@gmail.com>
2023-04-25 17:59:31 -0500

    Font updates (fix font weights)

--- src/components/MailListContent/MailListContent.js
+++ src/components/MailListContent/MailListContent.js
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import Spinner from '../Spinner/Spinner';
-import './MailListContent.css'
+import React, { useState, useEffect } from "react";
+import Spinner from "../Spinner/Spinner";
+import "./MailListContent.css";
 
 /* Get window size for scaling iFrames */
 function getWindowSize() {
-    const {innerWidth, innerHeight} = window;
-    return {innerWidth, innerHeight};
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
 }
 
 function MailListContent() {
-  
-  const [windowSize, setWindowSize] = useState(getWindowSize()); /* Window size State */
-  
+  const [windowSize, setWindowSize] = useState(
+    getWindowSize()
+  ); /* Window size State */
+
   /* On window resize (and page load), set iFrame & Newsletter preview height */
   /* Mainly optimized for the Google Form, to avoid scrolling within the iFrame */
   useEffect(() => {
@@ -19,50 +20,56 @@ function MailListContent() {
       setWindowSize(getWindowSize());
     }
 
-    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener("resize", handleWindowResize);
 
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener("resize", handleWindowResize);
     };
   }, [windowSize]);
-  
+
   let iFrameHeight = "1750";
   let previewHeight = "1700";
-  if (windowSize.innerWidth <= 320){
+  if (windowSize.innerWidth <= 320) {
     iFrameHeight = "2500";
     previewHeight = "2450";
-  }
-  else if (windowSize.innerWidth <= 380){
+  } else if (windowSize.innerWidth <= 380) {
     iFrameHeight = "2200";
     previewHeight = "2150";
-  }
-  else if (windowSize.innerWidth <= 500){
+  } else if (windowSize.innerWidth <= 500) {
     iFrameHeight = "2000";
     previewHeight = "1950";
-  }
-  else if (windowSize.innerWidth < 768){
+  } else if (windowSize.innerWidth < 768) {
     iFrameHeight = "1850";
     previewHeight = "1800";
   }
 
-  
-  const [previewURL, setPreviewURL] = useState(""); /* URL for most recent newsletter */
-  const [previewLoading, setPreviewLoading] = useState(true); /* newletter preview loading state */
-  
+  const [previewURL, setPreviewURL] =
+    useState(""); /* URL for most recent newsletter */
+  const [previewLoading, setPreviewLoading] =
+    useState(true); /* newletter preview loading state */
+
   /* Get most recent newsletter URL from mailchimp */
   useEffect(() => {
-    fetch("https://api.allorigins.win/get?url=" + encodeURIComponent("https://us3.campaign-archive.com/home/?u=9725fd8683bb429d5d3d28d33&id=1c72884554"))
+    fetch(
+      "https://api.allorigins.win/get?url=" +
+        encodeURIComponent(
+          "https://us3.campaign-archive.com/home/?u=9725fd8683bb429d5d3d28d33&id=1c72884554"
+        )
+    )
       .then(function (response) {
         //console.log(response);
         return response.json();
       })
       .then(function (data) {
         //console.log(data.contents);
-        let theURL = data.contents.match(/<li class="campaign">\d*\/\d*\/\d* - <a href="(.*?)"/)[1]; /* Pull string matching this regex to extract URL */
+        let theURL = data.contents.match(
+          /<li class="campaign">\d*\/\d*\/\d* - <a href="(.*?)"/
+        )[1]; /* Pull string matching this regex to extract URL */
         //console.log(theURL);
-        
+
         // console.log(theURL);
-        theURL = theURL.slice(0,4) + "s" + theURL.slice(4); /* Change URL to https */
+        theURL =
+          theURL.slice(0, 4) + "s" + theURL.slice(4); /* Change URL to https */
         // console.log(theURL);
         setPreviewURL(theURL);
         setPreviewLoading(false);
@@ -77,36 +84,39 @@ function MailListContent() {
       </div>
       <div class="mail-list">
         <div class="gform-wrapper">
-            <iframe
-                class="gform"
-                src="https://docs.google.com/forms/d/e/1FAIpQLScaahgIdex5tTTZnRiWNXglj62eJ4t9Ut505KCtjWuo55IqTw/viewform?embedded=true"
-                // width="700"
-                height={iFrameHeight}
-                frameborder="0"
-                marginHeight="0"
-                marginWidth="0"
-                // scrolling='no'
-            >
-                Loading…
-            </iframe>
+          <iframe
+            class="gform"
+            src="https://docs.google.com/forms/d/e/1FAIpQLScaahgIdex5tTTZnRiWNXglj62eJ4t9Ut505KCtjWuo55IqTw/viewform?embedded=true"
+            // width="700"
+            height={iFrameHeight}
+            frameborder="0"
+            marginHeight="0"
+            marginWidth="0"
+            // scrolling='no'
+          >
+            Loading…
+          </iframe>
         </div>
-        {previewLoading ? <Spinner /> : 
-          <div class="mail-list-preview">
-              <iframe 
-                  class="mail-list-embed"
-                  src={previewURL}
-                  height={previewHeight}
-                  frameborder="0"
-                  marginHeight="0"
-                  marginWidth="0"
-              >
-                  Loading Newsletter Preview...
-              </iframe>
-          </div>
-        }
+
+        <div class="mail-list-preview">
+          {previewLoading ? (
+            <Spinner />
+          ) : (
+            <iframe
+              class="mail-list-embed"
+              src={previewURL}
+              height={previewHeight}
+              frameborder="0"
+              marginHeight="0"
+              marginWidth="0"
+            >
+              Loading Newsletter Preview...
+            </iframe>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
-export default MailListContent
+export default MailListContent;
