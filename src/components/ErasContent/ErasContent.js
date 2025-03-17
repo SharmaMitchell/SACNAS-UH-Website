9d0b7b0c4238413b68b81f5730479a81ad6bc529
Mitchell Sharma <sharmamitch@gmail.com>
2025-03-17 15:33:43 -0500

    Add eras page content from article

--- src/components/ErasContent/ErasContent.js
+++ src/components/ErasContent/ErasContent.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useArticleData } from "../../hooks/useArticleData";
+import ArticleUi from "../ArticleContent/ArticleUI";
 
 function ErasContent() {
+  const articleId = "1hOzkW5A4Ynd9ywJFwzTqmNUENGih2D6-U7kvzC-nnWk";
+
+  const {
+    articleData,
+    articleMetadata,
+    articleDataLoading,
+    articleMetadataLoading,
+  } = useArticleData(articleId);
   return (
-    <div>
-      <h1>Eras</h1>
-      <p>
-        The Eras page is currently under construction. Please check back later
-        for updates.
-      </p>
-    </div>
+    <ArticleUi
+      articleData={articleData}
+      articleMetadata={articleMetadata}
+      articleDataLoading={articleDataLoading}
+      articleMetadataLoading={articleMetadataLoading}
+    />
   );
 }
 
