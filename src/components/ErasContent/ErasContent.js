e15cae8707343ddc030666ff2fa96f0e4cf72209
Mitchell Sharma <sharmamitch@gmail.com>
2025-03-17 15:34:29 -0500

    TODO

--- src/components/ErasContent/ErasContent.js
+++ src/components/ErasContent/ErasContent.js
@@ -3,6 +3,7 @@ import { useArticleData } from "../../hooks/useArticleData";
 import ArticleUi from "../ArticleContent/ArticleUI";
 
 function ErasContent() {
+  // TODO: Update article to new ERAS article once available
   const articleId = "1hOzkW5A4Ynd9ywJFwzTqmNUENGih2D6-U7kvzC-nnWk";
 
   const {
