c481b92b50cb91b46a6de9d553897ee59388c8bf
Mitchell Sharma <sharmamitch@gmail.com>
2025-03-17 15:30:15 -0500

    WIP define eras content

---
+++ src/components/ErasContent/ErasContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ErasContent() {
+  return (
+    <div>
+      <h1>Eras</h1>
+      <p>
+        The Eras page is currently under construction. Please check back later
+        for updates.
+      </p>
+    </div>
+  );
+}
+
+export default ErasContent;
