f8229ff6e07dd00ba5ff7b2d43d63e27858496b4
Mitchell Sharma <sharmamitch@gmail.com>
2025-03-17 15:25:10 -0500

    Add ERAS page

---
+++ src/pages/Eras.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import ErasContent from "../components/ErasContent/ErasContent";
+
+function Resources() {
+  useEffect(() => {
+    document.title =
+      "Resources - SACNAS UH | Celebrating Scientific Research and Diversity in STEM";
+  }, []);
+  return (
+    <>
+      <ErasContent />
+    </>
+  );
+}
+
+export default Resources;
